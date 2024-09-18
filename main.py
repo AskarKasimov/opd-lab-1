1ba2cf494c05c8a2064ed263408cac72afdde23d
Askar Kasimov <kasimovaskar123@gmail.com>
2024-09-18 22:33:50 +0300

    2nd task

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
 import sys
-commands = []
-with open("task.txt", "r", encoding="utf-8") as file:
-    lines = file.readlines()
-    for line in range(len(lines)):
-        name = lines[line].split()[-2][3:]
-        type = lines[line].split()[-1]
-        print(lines[line].split())
-        if line > 0 and len(lines[line-1].split()) > len(lines[line].split()):
-            command = "cd " + "../" * (len(lines[line-1].split()) - len(lines[line].split()))
-            commands.append(command)
-        elif line > 0 and len(lines[line-1].split()) < len(lines[line].split()):
-            command = f"cd {lines[line-1].split()[-2][3:]}"
-            commands.append(command)
-        if type == "каталог":
-            command = f"mkdir {name}"
-        elif type == "файл":
-            command = f"touch {name}"
-        else:
-            print(f"unknown type: {type}")
-            sys.exit()
-        commands.append(command)
+import logging
+from task2.main import Content
 
-with open("solve.sh", "w") as file:
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+commands = []
+content = Content()
+
+with open("task1.txt", "r", encoding="utf-8") as file:
+    lines = file.readlines()
+    for line_index in range(len(lines)):
+        this_line = lines[line_index]
+        this_name = lines[line_index].split()[-2][3:]
+        this_type = lines[line_index].split()[-1]
+
+        prev_line = lines[line_index-1]
+        prev_name = lines[line_index-1].split()[-2][3:]
+
+        if line_index > 0 and len(prev_line.split()) > len(this_line.split()):
+            commands.append("cd " +
+                            "../" * (len(prev_line.split()) - len(this_line.split())))
+        elif line_index > 0 and len(prev_line.split()) < len(this_line.split()):
+            commands.append(f"cd {prev_name}")
+
+        if this_type == "каталог":
+            commands.append(f"mkdir {this_name}")
+        elif this_type == "файл":
+            commands.append(f"touch {this_name}")
+            try:
+                commands.append(
+                    f"echo -e '{content.get_content_by_filename(this_name).replace("\n", "\\n")}' > {this_name}")
+            except KeyError:
+                logging.info(f"File not found: {this_name}")
+        else:
+            logging.error(f"Unknown type in parsed file: {this_type}")
+            sys.exit()
+
+with open("solve.sh", "w", encoding="utf-8") as file:
     for command in commands:
         file.write(command + "\n")
---
+++ solve.sh
@@ -0,0 +1,220 @@
+mkdir archeops4
+cd archeops4
+touch charizard
+echo -e 'weigth=199.5 height=67.0 atk=8 def=8' > charizard
+mkdir trapinch
+cd trapinch
+mkdir eelektross
+cd eelektross
+mkdir staryu
+mkdir arbok
+cd ../
+mkdir sealeo
+cd sealeo
+touch slowbro
+echo -e 'Ходы  After\nYou Aqua Tail Avalanche Block Body Slam Brine Counter Dive Double-Edge\nDrain Punch Dynamicpunch Focus Punch Foul Play Fury Cutter Ice Punch\nIcy Wind Iron Defense Iron Tail Magic Coat Mega Kick Mega Punch\nMud-Slap Recycle Role Play Seismic Toss Signal Beam Skill Swap Snore\nSwift Trick Wonder Room Zen Headbutt' > slowbro
+touch doduo
+echo -e 'Развитые способности\nTangled Feet' > doduo
+mkdir abomasnow
+touch simisage
+echo -e 'Живет  Forest Rainforest' > simisage
+cd ../
+mkdir gible
+cd gible
+mkdir steelix
+mkdir rapidash
+touch medicham
+echo -e 'Способности\nFocus Mind Mold Pure Power Inner Focus' > medicham
+touch elekid
+echo -e 'Развитые способности\nVital Spirit' > elekid
+touch cranidos
+echo -e 'Тип диеты  Herbivore' > cranidos
+cd ../
+touch scizor
+echo -e 'Развитые\nспособности  Light Metal Steadfast' > scizor
+touch rufflet
+echo -e 'Тип диеты\nCarnivore' > rufflet
+touch deino
+echo -e 'Способности  Dark Art Pure Blooded Hustle\nKlutz' > deino
+mkdir palpitoad
+cd palpitoad
+mkdir huntail
+touch umbreon
+echo -e 'Ходы Body Slam Covet Dark Pulse Double-Edge Foul Play\nHeal Bell Helping Hand Hyper Voice Iron Tail Last Resort Mud-Slap Role\nPlay Sleep Talk Snatch Snore Spite Sucker Punch Swift Wonder\nRoom' > umbreon
+touch omastar
+echo -e 'Ходы Ancientpower Bind Body Slam Dive Double-Edge Earth\nPower Icy Wind Iron Defense Knock Off Mud-Slap Rollout Seismic Toss\nSleep Talk Snore Stealth Rock Water Pulse' > omastar
+cd ../../
+mkdir sandshrew
+cd sandshrew
+mkdir persian
+cd persian
+mkdir glalie
+mkdir blastoise
+cd ../
+touch ampharos
+echo -e 'Ходы After You\nBody Slam Counter Defense Curl Double-Edge Dynamicpunch Fire Punch‡\nFocus Punch Heal Bell Iron Tail Magnet Rise Mega Kick Mega Punch\nOutrage Seismic Toss Shock Wave Signal Beam Sleep Talk Snore Swift\nThunderpunch' > ampharos
+touch magmortar
+echo -e 'weigth=149.9 height=63.0 atk=10\ndef=7' > magmortar
+mkdir exploud
+cd exploud
+mkdir spoink
+mkdir lickilicky
+touch houndoom
+echo -e 'Развитые способности  Unnerve' > houndoom
+mkdir dustox
+touch wailmer
+echo -e 'Ходы  Avalanche\nBounce Dive Hyper Voice Icy Wind Rollout Sleep Talk Snore Zen\nHeadbutt' > wailmer
+touch shelmet
+echo -e 'Способности Swarm Hydration Shell.\nArmor' > shelmet
+touch onix
+echo -e 'Развитые способности Weak Armor' > onix
+cd ../../
+touch espeon
+echo -e 'Тип покемона\nPSYCHIC NONE' > espeon
+touch seaking
+echo -e 'Тип диеты  Herbivore' > seaking
+touch poliwhirl
+echo -e 'Способности\nTorrent Damp Water Absorb' > poliwhirl
+cd ../
+touch chandelurе6
+echo -e 'Тип диеты\nNullivore' > chandelurе6
+touch feraligatr6
+echo -e 'weigth=195.8 height=91.0 atk=11\ndef=10' > feraligatr6
+mkdir houndoom1
+cd houndoom1
+mkdir crawdaunt
+cd crawdaunt
+mkdir zoroark
+cd zoroark
+mkdir infernape
+touch crustle
+echo -e 'satk 7 sdef=8 spd=5' > crustle
+touch ampharos
+echo -e 'Живет  Grassland\nMountain' > ampharos
+mkdir samurott
+touch purrloin
+echo -e 'weigth=22.3 height=16.0 atk=5\ndef=4' > purrloin
+cd ../
+mkdir sudowoodo
+cd sudowoodo
+touch hypno
+echo -e 'Способности  Mind Mold Insomnia Forewarn' > hypno
+touch crawdaunt
+echo -e 'Тип\nпокемона  WATER DARK' > crawdaunt
+mkdir teddiursa
+cd ../
+mkdir whiscash
+cd whiscash
+touch shellder
+echo -e 'Тип диеты  Herbivore' > shellder
+touch hoothoot
+echo -e 'Живет\nForest' > hoothoot
+mkdir xatu
+mkdir ninjask
+cd ../
+touch beedrill
+echo -e 'weigth=65.0 height=39.0 atk=8 def=4' > beedrill
+touch mantyke
+echo -e 'Развитые\nспособности  Water Veil' > mantyke
+touch kirlia
+echo -e 'Способности  Confusion Double Team\nTeleport Lucky Chant Magical Leaf Heal Pulse Calm Mind Psychic\nImprison Future Sight Charm Hypnosis Dream Eater Stored\nPower' > kirlia
+cd ../
+touch stunky
+echo -e 'Способности  Venom Dark Art Stench\nAftermath' > stunky
+mkdir hippopotas
+cd hippopotas
+mkdir octillery
+cd octillery
+mkdir tirtouga
+touch cubchoo
+echo -e 'weigth=18.7 height=20.0 atk=7 def=4' > cubchoo
+mkdir weepinbell
+touch mantine
+echo -e 'satk=8\nsdef=14 spd=7' > mantine
+touch chimchar
+echo -e 'Способности  Leer Scratch Ember Taunt Fury\nSwipes Flame Wheel Nasty Plot Torment Facade Fire Spin Acrobatics\nSlack Off Flamethrower' > chimchar
+cd ../
+mkdir camerupt
+cd camerupt
+touch shelmet
+echo -e 'Развитые способности\nOvercoat' > shelmet
+mkdir electrike
+touch remoraid
+echo -e 'satk 7 sdef=4 spd=7' > remoraid
+mkdir claydol
+cd ../../../
+touch jolteon4
+echo -e 'Живет  Forest\nUrban' > jolteon4
+mkdir magmortar0
+cd magmortar0
+touch tangrowth
+echo -e 'Способности  Sleep Powder Vine Whip Absorb\nPoisonpowder Bind Growth Mega Drain Knock Off Stun Spore Natural Gift\nGiga Drain Ancientpower Slam Tickle Wring Out Power Whip\nBlock' > tangrowth
+mkdir lileep
+cd lileep
+mkdir hariyama
+cd hariyama
+mkdir porygon
+mkdir crobat
+mkdir weedle
+mkdir sentret
+cd ../
+mkdir sneasel
+cd sneasel
+mkdir cleffa
+touch camerupt
+echo -e 'Способности  Ember Magnitude Focus Energy Flame Burst\nAmnesia Lava Plume Earth Power Curse Take Down Rock Slide Yawn\nEarthquake Eruption Fissure' > camerupt
+touch weezing
+echo -e 'Развитые способности\nAftermath' > weezing
+cd ../../
+mkdir venipede
+cd venipede
+touch torchic
+echo -e 'Способности  Blaze Klutz Moxie' > torchic
+touch dustox
+echo -e 'Возможности\nOverland=2 Sky=9 Jump=3 Power=3 Intelligence=4\nSinker=0' > dustox
+mkdir cottonee
+cd cottonee
+mkdir ralts
+mkdir chimecho
+touch hippowdon
+echo -e 'Возможности  Overland=6 Surface=6 Underwater=4\nBurrow6=0 Jump=3 Power=4 Intelligence=4\nGroundshaper=0' > hippowdon
+mkdir boldore
+cd ../../
+touch pineco
+echo -e 'weigth=15.9 height=24.0 atk=7\ndef=9' > pineco
+mkdir spheal
+cd spheal
+mkdir lotad
+cd lotad
+touch gallade
+echo -e 'satk=7 sdef=12 spd=8' > gallade
+touch gengar
+echo -e 'Способности  Haunt Venom\nLevitate Intimidate' > gengar
+mkdir voltorb
+touch cyndaquil
+echo -e 'weigth=17.4 height=20.0 atk=5 def=4' > cyndaquil
+cd ../
+mkdir horsea
+cd horsea
+touch arbok
+echo -e 'Тип покемона  POISON NONE' > arbok
+mkdir pignite
+touch darmanitan
+echo -e 'Способности\nRollout Incinerate Rage Fire Fang Headbutt Swagger Facade Fire Punch\nWork Up Thrash Belly Drum Flare Blitz Hammer Arm Taunt Superpower\nOverheat' > darmanitan
+touch lickitung
+echo -e 'weigth=144.4 height=47.0 atk=6\ndef=8' > lickitung
+mkdir vanillish
+cd ../
+mkdir venipede
+cd venipede
+touch samurott
+echo -e 'satk=8 sdef=7 spd=7' > samurott
+touch donphan
+echo -e 'satk 6 sdef=6\nspd=5' > donphan
+touch whirlpede
+echo -e 'Живет  Forest' > whirlpede
+touch lanturn
+echo -e 'Ходы  Aqua Tail Bounce Brine\nDive Double-Edge Heal Bell Icy Wind Shock Wave Signal Beam Snore\nSucker Punch Water Pulse' > lanturn
+cd ../
+touch pineco
+echo -e 'weigth=15.9 height=24.0 atk=7 def=9' > pineco
